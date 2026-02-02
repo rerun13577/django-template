548ab7472f390941b500596ceeed77c3df7d55c2
rerun13577 <evonelin52@gmail.com>
2026-02-02 23:04:48 +0800

    feat: show database items on home page

--- aquatic/views.py
+++ aquatic/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from aquatic.models import AquaticLife # 引入你的模型
 
-# Create your views here.
+def index(request):
+    # 撈出資料庫裡所有的水生生物
+    items = AquaticLife.objects.all()
+    
+    # 把這筆資料打包傳給網頁
+    return render(request, 'index.html', {'items': items})
